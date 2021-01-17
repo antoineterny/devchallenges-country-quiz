fbbf91d08ffd96c61fd7d8922e75c331d73e1b0a
Antoine Terny <antoine.terny@gmail.com>
2021-01-18 00:03:08 +0100

    fonctionnel

--- src/App.js
+++ src/App.js
@@ -7,100 +7,112 @@ import StartPage from "./StartPage"
 import Results from "./Results"
 
 class App extends React.Component {
-  state = {
-    countries: [],
-    region: "", // Africa Americas Asia Europe Oceania
-    difficulty: 50, // most populous in list
-    nbQuestions: 20,
-    questions: [],
-    quizType: "capital",
-    turn: 0,
-    score: 0,
-  }
-  initialState = this.state
+	state = {
+		countries: [],
+		questions: [],
+		quizType: "",
+		region: "", // Africa Americas Asia Europe Oceania
+		nbQuestions: null,
+		difficulty: null, // most populous in list
+		turn: 0,
+		score: 0
+	}
+	initialState = this.state
 
-  componentDidMount() {
-    this.prepareQuestions()
-  }
+	async prepareQuestions() {
+		console.log(this.state)
+		const base = "https://restcountries.eu/rest/v2/"
+		const region =
+			this.state.region === "World" ? "all/" : "region/" + this.state.region
+		const fields = "?fields=name;capital;flag;population"
+		let countries = await fetch(base + region + fields)
+		countries = await countries.json()
+		countries = await countries
+			.sort((a, b) => (a.population < b.population ? 1 : -1))
+			.splice(0, this.state.difficulty)
+		this.setState({ countries: countries })
+		const questions = [...countries]
+		this.shuffle(questions).splice(this.state.nbQuestions, questions.length - 1)
+		this.setState({ questions: questions })
+	}
 
-  async prepareQuestions() {
-    const base = "https://restcountries.eu/rest/v2/"
-    const region = this.state.region === "" ? "all/" : "region/" + this.state.region
-    const fields = "?fields=name;capital;flag;population"
-    let countries = await fetch(base + region + fields)
-    countries = await countries.json()
-    countries = await countries
-      .sort((a, b) => (a.population < b.population ? 1 : -1))
-      .splice(0, this.state.difficulty)
-    this.setState({ countries: countries })
-    const questions = [...countries]
-    this.shuffle(questions).splice(this.state.nbQuestions, questions.length - 1)
-    this.setState({ questions: questions })
-  }
+	onStartSubmit = (e, { type, region, nbQuestions, difficulty }) => {
+		e.preventDefault()
+		this.setState({
+			countries: [],
+			questions: [],
+			quizType: type,
+			region: region,
+			nbQuestions: nbQuestions,
+			difficulty: difficulty,
+			turn: 1,
+			score: 0
+		})
+		setTimeout(() => this.prepareQuestions(), 100)
+	}
 
-  onStartSubmit = (e, type) => {
-    e.preventDefault()
-    console.log(type)
-    this.setState({ quizType: type, turn: 1 })
-  }
+	nextQuestion = () => {
+		this.setState({ turn: this.state.turn + 1 })
+	}
+	incrementScore = () => this.setState({ score: this.state.score + 1 })
 
-  nextQuestion = () => {
-    this.setState({ turn: this.state.turn + 1 })
-  }
-  incrementScore = () => this.setState({ score: this.state.score + 1 })
+	initializeState = () => {
+		this.setState(this.initialState)
+		this.prepareQuestions()
+	}
 
-  initializeState = () => {
-    this.setState(this.initialState)
-    this.prepareQuestions()
-  }
+	// Fisher–Yates shuffle
+	shuffle(array) {
+		var currentIndex = array.length,
+			temporaryValue,
+			randomIndex
+		while (0 !== currentIndex) {
+			randomIndex = Math.floor(Math.random() * currentIndex)
+			currentIndex -= 1
+			temporaryValue = array[currentIndex]
+			array[currentIndex] = array[randomIndex]
+			array[randomIndex] = temporaryValue
+		}
+		return array
+	}
 
-  // Fisher–Yates shuffle
-  shuffle(array) {
-    var currentIndex = array.length,
-      temporaryValue,
-      randomIndex
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex)
-      currentIndex -= 1
-      temporaryValue = array[currentIndex]
-      array[currentIndex] = array[randomIndex]
-      array[randomIndex] = temporaryValue
-    }
-    return array
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <div className="quiz-header">
-          <h1>Country quiz</h1>
-          {this.state.turn < this.state.nbQuestions + 1 ? (
-            <LogoTravel className="svg-travel" />
-          ) : (
-            <LogoResults className="svg-results" />
-          )}
-        </div>
-        <div className="quiz-body">
-          {this.state.turn === 0 ? (
-            <StartPage onStartSubmit={this.onStartSubmit} />
-          ) : this.state.turn === this.state.nbQuestions + 1 ? (
-            <Results score={this.state.score} initializeState={this.initializeState} />
-          ) : (
-            <Question
-              countries={this.state.countries}
-              questions={this.state.questions}
-              quizType={this.state.quizType}
-              turn={this.state.turn}
-              nbQuestions={this.state.nbQuestions}
-              shuffle={this.shuffle}
-              nextQuestion={this.nextQuestion}
-              incrementScore={this.incrementScore}
-            />
-          )}
-        </div>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="quiz-header">
+					<h1>Country quiz</h1>
+					{this.state.turn < this.state.nbQuestions + 1 ? (
+						<LogoTravel className="svg-travel" />
+					) : (
+						<LogoResults className="svg-results" />
+					)}
+				</div>
+				<div className="quiz-body">
+					{this.state.turn === 0 ? (
+						<StartPage onStartSubmit={this.onStartSubmit} />
+					) : this.state.turn === this.state.nbQuestions + 1 ? (
+						<Results
+							score={this.state.score}
+							initializeState={this.initializeState}
+						/>
+					) : this.state.questions.length < this.state.nbQuestions ? (
+						<p>charging...</p>
+					) : (
+						<Question
+							countries={this.state.countries}
+							questions={this.state.questions}
+							quizType={this.state.quizType}
+							turn={this.state.turn}
+							nbQuestions={this.state.nbQuestions}
+							shuffle={this.shuffle}
+							nextQuestion={this.nextQuestion}
+							incrementScore={this.incrementScore}
+						/>
+					)}
+				</div>
+			</div>
+		)
+	}
 }
 
 export default App
--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -1,38 +1,171 @@
+import "./StartPage.scss"
 import React from "react"
 
 class StartPage extends React.Component {
-  state = { type: "capital" }
-  onChangeHandler(e) {
-    this.setState({ type: e.target.value })
-  }
-  render() {
-    return (
-      <form onSubmit={e => this.props.onStartSubmit(e, this.state.type)}>
-        <input
-          type="radio"
-          id="capital"
-          name="type"
-          value="capital"
-          onChange={e => this.onChangeHandler(e)}
-          defaultChecked
-        />
-        <label htmlFor="capital">Capital quiz</label>
-        <br />
-        <br />
-        <input
-          type="radio"
-          id="flag"
-          name="type"
-          value="flag"
-          onChange={e => this.onChangeHandler(e)}
-        />
-        <label htmlFor="flag">Flag quiz</label>
-        <br />
-        <br />
-        <input type="submit" value="Start Quiz" />
-      </form>
-    )
-  }
+	state = { type: "capital", region: "World", nbQuestions: 5, difficulty: 20 }
+	onTypeChange(e) {
+		this.setState({ type: e.target.value })
+	}
+	onRegionChange(e) {
+		this.setState({ region: e.target.value })
+	}
+	onNumberChange(e) {
+		this.setState({ nbQuestions: e.target.value })
+	}
+	onDifficultyChange(e) {
+		this.setState({ difficulty: e.target.value })
+	}
+	render() {
+		return (
+			<form
+				className="StartPage-form"
+				onSubmit={e => this.props.onStartSubmit(e, this.state)}
+			>
+				<h2>Quiz type</h2>
+				<div className="fieldset">
+					<input
+						type="radio"
+						id="capital"
+						name="type"
+						value="capital"
+						onChange={e => this.onTypeChange(e)}
+						defaultChecked
+					/>
+					<label htmlFor="capital">Capital quiz</label>
+					<input
+						type="radio"
+						id="flag"
+						name="type"
+						value="flag"
+						onChange={e => this.onTypeChange(e)}
+					/>
+					<label htmlFor="flag">Flag quiz</label>
+				</div>
+
+				<h2>Region</h2>
+				<div className="fieldset">
+					<input
+						type="radio"
+						name="region"
+						id="World"
+						value="World"
+            onChange={e => this.onRegionChange(e)}
+            defaultChecked
+					/>
+					<label htmlFor="World">World</label>
+
+					<input
+						type="radio"
+						name="region"
+						id="Africa"
+						value="Africa"
+						onChange={e => this.onRegionChange(e)}
+					/>
+					<label htmlFor="Africa">Africa</label>
+
+					<input
+						type="radio"
+						name="region"
+						id="Americas"
+						value="Americas"
+						onChange={e => this.onRegionChange(e)}
+					/>
+					<label htmlFor="Americas">Americas</label>
+
+					<input
+						type="radio"
+						name="region"
+						id="Asia"
+						value="Asia"
+						onChange={e => this.onRegionChange(e)}
+					/>
+					<label htmlFor="Asia">Asia</label>
+
+					<input
+						type="radio"
+						name="region"
+						id="Europe"
+						value="Europe"
+						onChange={e => this.onRegionChange(e)}
+					/>
+					<label htmlFor="Europe">Europe</label>
+
+					<input
+						type="radio"
+						name="region"
+						id="Oceania"
+						value="Oceania"
+						onChange={e => this.onRegionChange(e)}
+					/>
+					<label htmlFor="Oceania">Oceania</label>
+				</div>
+
+        <h2>Number of questions</h2>
+				<div className="fieldset">
+					<input
+						type="radio"
+						name="number"
+						id="5"
+						value="5"
+						onChange={e => this.onNumberChange(e)}
+						defaultChecked
+					/>
+					<label htmlFor="5">5</label>
+
+					<input
+						type="radio"
+						name="number"
+						id="10"
+						value="10"
+						onChange={e => this.onNumberChange(e)}
+					/>
+					<label htmlFor="10">10</label>
+
+					<input
+						type="radio"
+						name="number"
+						id="20"
+						value="20"
+						onChange={e => this.onNumberChange(e)}
+					/>
+					<label htmlFor="20">20</label>
+				</div>
+
+        <h2>Difficulty</h2>
+				<div className="fieldset">
+					<input
+						type="radio"
+						name="difficulty"
+						id="easy"
+						value="20"
+						onChange={e => this.onDifficultyChange(e)}
+						defaultChecked
+					/>
+					<label htmlFor="easy">easy</label>
+
+					<input
+						type="radio"
+						name="difficulty"
+						id="normal"
+						value="50"
+						onChange={e => this.onDifficultyChange(e)}
+					/>
+					<label htmlFor="normal">normal</label>
+
+					<input
+						type="radio"
+						name="difficulty"
+						id="hard"
+						value="150"
+						onChange={e => this.onDifficultyChange(e)}
+					/>
+					<label htmlFor="hard">hard</label>
+				</div>
+
+				<input type="submit" value="Start Quiz" />
+			</form>
+		)
+	}
 }
 
 export default StartPage
